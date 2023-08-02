53adccaff5b8a43f34bb3fae7ba141e15fab8fc9
mdmahedihassanshuvo <mdmahedihassanshuvo8@gmail.com>
2023-08-02 22:53:46 +0600

    footer added

--- src/Shared/Footer/Footer.jsx
+++ src/Shared/Footer/Footer.jsx
@@ -1,10 +1,42 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
     return (
-        <div>
-            this is footer
-        </div>
+        <footer className="footer grid grid-cols-2 lg:grid-cols-none p-10 bg-stone-800 text-slate-300">
+            <div>
+                <span className="footer-title">Services</span>
+                <Link className="link link-hover">Branding</Link>
+                <Link className="link link-hover">Design</Link>
+                <Link className="link link-hover">Marketing</Link>
+                <Link className="link link-hover">Advertisement</Link>
+            </div>
+            <div>
+                <span className="footer-title">Company</span>
+                <Link className="link link-hover">About us</Link>
+                <Link className="link link-hover">Contact</Link>
+                <Link className="link link-hover">Jobs</Link>
+                <Link className="link link-hover">Press kit</Link>
+            </div>
+            <div>
+                <span className="footer-title">Legal</span>
+                <Link className="link link-hover">Terms of use</Link>
+                <Link className="link link-hover">Privacy policy</Link>
+                <Link className="link link-hover">Cookie policy</Link>
+            </div>
+            <div className='col-span-full'>
+                <span className="footer-title">Newsletter</span>
+                <div className="form-control w-80">
+                    <label className="label">
+                        <span className="label-text text-slate-300">Enter your email address</span>
+                    </label>
+                    <div className="relative">
+                        <input type="text" placeholder="username@site.com" className="input input-bordered text-black w-full pr-16" />
+                        <button className="btn btn-primary absolute top-0 right-0 rounded-l-none">Subscribe</button>
+                    </div>
+                </div>
+            </div>
+        </footer>
     );
 };
 
