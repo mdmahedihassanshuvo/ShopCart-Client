7f106a46a4fdce3f79dd02392dbe5972408dfac3
mdmahedihassanshuvo <mdmahedihassanshuvo8@gmail.com>
2023-08-02 08:44:35 +0600

    banner added in home page

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,18 +1,18 @@
-// import React from 'react';
-// import { Helmet } from 'react-helmet-async';
-// import Banner from '../Components/Banner/Banner';
-// import Headphone from '../../HeadPhones/Headphone';
+import React from 'react';
+import { Helmet } from 'react-helmet-async';
+import Banner from '../Components/Banner/Banner';
+import Headphone from '../../HeadPhones/Headphone';
 
-// const Home = () => {
-//     return (
-//         <div>
-//             <Helmet>
-//                 <title>ShopCart-Home</title>
-//             </Helmet>
-//             <Banner/>
-//             <Headphone/>
-//         </div>
-//     );
-// };
+const Home = () => {
+    return (
+        <div>
+            <Helmet>
+                <title>ShopCart-Home</title>
+            </Helmet>
+            <Banner/>
+            <Headphone/>
+        </div>
+    );
+};
 
-// export default Home;
+export default Home;
