7ab8c8385ef65d1bc3f57f62486172b2056ff8f8
mdmahedihassanshuvo <mdmahedihassanshuvo8@gmail.com>
2023-08-02 21:13:59 +0600

    display headphone get from mongodb and show in ui

--- src/Pages/HeadPhones/Headphone.jsx
+++ src/Pages/HeadPhones/Headphone.jsx
@@ -1,9 +1,45 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AuthContext } from '../../AuthProvider/AuthProvider';
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
 
 const Headphone = () => {
+
+    const { loading } = useContext(AuthContext);
+
+    const { data: displayHeadphones = [], refetch } = useQuery({
+        queryKey: ['displayHeadphones'],
+        // enabled: !loading,
+        queryFn: async () => {
+            const res = await axios.get('http://localhost:5000/display')
+            console.log(res.data)
+            return res.data
+        }
+    })
+    console.log(displayHeadphones)
+
     return (
-        <div>
-            <h1 className='text-3xl font-semibold'>Headphones for you !</h1>
+        <div className='lg:my-10'>
+            <h1 className='text-3xl font-semibold lg:mb-16'>Headphones for you !</h1>
+            <div className='grid grid-cols-1 lg:grid-cols-3 gap-5'>
+                {
+                    displayHeadphones.map((headphone) => (
+                        <div className="card card-compact w-96 bg-base-100 shadow-xl">
+                            <figure><img className='lg:h-[257px] lg:w-[384px]' src={headphone.image} alt="headphone" /></figure>
+                            <div className="card-body">
+                                <div className=''>
+                                    <h2 className="card-title">${headphone.name}</h2>
+                                    <p className="text-lg font-medium">price: ${headphone.price}</p>
+                                </div>
+                                <p>{headphone.details}</p>
+                                <div className="card-actions justify-start">
+                                    <button className="btn bg-white border border-black hover:bg-[#003c2a] hover:text-white transition-colors">Add to Cart</button>
+                                </div>
+                            </div>
+                        </div>
+                    ))
+                }
+            </div>
         </div>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -5,15 +5,19 @@ import { RouterProvider } from 'react-router-dom'
 import router from './Routes/Route/Route.jsx'
 import { HelmetProvider } from 'react-helmet-async';
 import AuthProvider from './AuthProvider/AuthProvider'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <HelmetProvider>
-      <div className='lg:mx-10'>
-        <AuthProvider>
-          <RouterProvider router={router} />
-        </AuthProvider>
-      </div>
+      <AuthProvider>
+        <QueryClientProvider client={queryClient}>
+          <div className='lg:mx-10'>
+            <RouterProvider router={router} />
+          </div>
+        </QueryClientProvider>
+      </AuthProvider>
     </HelmetProvider>
   </React.StrictMode>,
 )
